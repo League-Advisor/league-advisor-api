6560badf582a7989ef31ab7855ed34b477b49fc4
duajaradat <duajaradat164@gmail.com>
2021-12-12 11:20:37 +0200

    refactor: _automate populating items

--- champions/management/commands/populate_champions.py
+++ champions/management/commands/populate_champions.py
@@ -4,8 +4,8 @@ from champions.models import Champion
 import requests
 import time
 
-class Command(BaseCommand):
 
+class Command(BaseCommand):
     def handle(self, *args, **options):
 
         # NOTE: Remove all old model objects
@@ -16,7 +16,7 @@ class Command(BaseCommand):
         time.sleep(10)
 
         # NOTE: GET THE LATEST VERSION
-        with open('static/versions.json') as local_versions:
+        with open("static/versions.json") as local_versions:
             local_version = json.load(local_versions)
 
         # NOTE: GET ALL CHAMPIONS JSON FILE FROM RIOT API
@@ -25,45 +25,59 @@ class Command(BaseCommand):
 
         counter = 0
         # NOTE: LOOP OVER EACH CHAMPION
-        for champion in json_file['data']:
+        for champion in json_file["data"]:
             # NOTE: GET EACH CHAMPION'S DETAILED JSON FILE FROM RIOT API
             champion_json_url = f"http://ddragon.leagueoflegends.com/cdn/{local_version[0]}/data/en_US/champion/{json_file['data'][champion]['id']}.json"
             champion_json = requests.get(champion_json_url).json()
 
             # NOTE: TRY NOT TO GET BLACKLISTED
-            time.sleep(2)   
+            time.sleep(1)
 
             # NOTE: GATHER NECESSARY SKIN INFO
             champion_skins = []
-            for skin in champion_json['data'][champion]['skins']:
-                champion_skins.append({skin["name"]:f'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/{json_file["data"][champion]["name"]}_{skin["num"]}.jpg'})                
-            
+            for skin in champion_json["data"][champion]["skins"]:
+                champion_skins.append(
+                    {
+                        skin[
+                            "name"
+                        ]: f'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/{json_file["data"][champion]["name"]}_{skin["num"]}.jpg'
+                    }
+                )
+
             # NOTE: GATHER NECESSARY SKILLS INFO
             champion_skills = []
-            for skill in champion_json['data'][champion]['spells']:
-                champion_skills.append({skill["name"]:f'http://ddragon.leagueoflegends.com/cdn/{local_version[0]}/img/spell/{skill["id"]}.png'})
+            for skill in champion_json["data"][champion]["spells"]:
+                champion_skills.append(
+                    {
+                        skill[
+                            "name"
+                        ]: f'http://ddragon.leagueoflegends.com/cdn/{local_version[0]}/img/spell/{skill["id"]}.png'
+                    }
+                )
 
             # NOTE: CREATE AN OBJECT
             # NOTE: STORE LISTS AS STR
 
-            print(f"POPULATING DATABSE WITH {champion_json['data'][champion]['name']} DATA" )
-            
-            Champion.objects.get_or_create(
-            pk=counter,
-            champion_id=champion_json['data'][champion]['id'],
-            key = champion_json['data'][champion]['key'],
-            name=champion_json['data'][champion]['name'],
-            title=champion_json['data'][champion]['title'],
-            lore=champion_json['data'][champion]['lore'],
+            print(
+                f"POPULATING DATABSE WITH {champion_json['data'][champion]['name']} DATA"
+            )
 
-            attack=json_file['data'][champion]['info']['attack'],
-            defense=json_file['data'][champion]['info']['defense'],
-            magic=json_file['data'][champion]['info']['magic'],
-            difficulty=json_file['data'][champion]['info']['difficulty'],
-            skills = str(champion_skills),
-            image=f"http://ddragon.leagueoflegends.com/cdn/img/champion/loading/{json_file['data'][champion]['name']}_0.jpg",
-            skins = str(champion_skins),
-            tags=json_file['data'][champion]['tags'],
-            stats=json_file['data'][champion]['stats'])
+            Champion.objects.get_or_create(
+                pk=counter,
+                champion_id=champion_json["data"][champion]["id"],
+                key=champion_json["data"][champion]["key"],
+                name=champion_json["data"][champion]["name"],
+                title=champion_json["data"][champion]["title"],
+                lore=champion_json["data"][champion]["lore"],
+                attack=json_file["data"][champion]["info"]["attack"],
+                defense=json_file["data"][champion]["info"]["defense"],
+                magic=json_file["data"][champion]["info"]["magic"],
+                difficulty=json_file["data"][champion]["info"]["difficulty"],
+                skills=str(champion_skills),
+                image=f"http://ddragon.leagueoflegends.com/cdn/img/champion/loading/{json_file['data'][champion]['name']}_0.jpg",
+                skins=str(champion_skins),
+                tags=json_file["data"][champion]["tags"],
+                stats=json_file["data"][champion]["stats"],
+            )
 
             counter += 1
